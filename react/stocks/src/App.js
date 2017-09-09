80a93dda785f111121817fc5fc7dc2d800d4f5df
Kaveh Khorram <kavehkhorram@huntsman-ve702-4291.apng.wlan.private.upenn.edu>
2017-09-09 11:12:30 -0400

    CORS

--- react/stocks/src/App.js
+++ react/stocks/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
             <Input onChange={this.updateAlgoText.bind(this)} s = {12} placeholder="If MSFT drops $10 from yesterday's close, buy 10 shares of MSFT" style={{"color": "white"}}/>
             <Button waves="light" onClick={this.updateCode}>Translate to Code</Button>
           </Row>
-          <MirrorTextWithHighlighting showText = {this.state.currentText}/>
+          <MirrorTextWithHighlighting showText = {this.highlightCurrentText()}/>
         </div>
     </div>
       </div>
@@ -39,11 +39,32 @@ class App extends Component {
   updateCode() {
     console.log("Code has been updated.");
     // Where the network request will go.
+    fetch('http://127.0.0.1:5000/api?text=if MSFT drops 20% from yesterday, buy 10 shares of AAPL.')
+    .then((value) => {
+      console.log('value: ' + value);
+    }).catch(error => {
+      console.log('error: ' + error);
+    })
   }
 
   updateAlgoText(obj, value) {
     console.log("Changing Algo Text");
     this.setState({currentText: value})
+  }
+
+  highlightCurrentText() {
+    // Split into words.
+    var tokenizeInput = this.state.currentText.split(" ");
+
+    // TODO: Make this better.
+    for (var i = 0; i < tokenizeInput.length; i++) {
+      // Go through all possible elements.
+      if (tokenizeInput[i].toLowerCase() === 'if') {
+        tokenizeInput[i] = "LMAO SHIT"
+      }
+    }
+
+    return tokenizeInput.join(" ");
   }
 }
 
--- react/stocks/src/components/MirrorTextWithHighlighting.js
+++ react/stocks/src/components/MirrorTextWithHighlighting.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import {Row, Input, Button, Col, CardPanel} from 'react-materialize';
 
 export default class MirrorTextWithHighlighting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentText: null
+        }
+    }
+
     render() {
         return (
-            <h2>{this.props.showText}</h2>
+            <h2>{this.state.currentText || this.props.showText}</h2>
         )
     }
 }
